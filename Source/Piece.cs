d0b9b6a7202f5f90307c0f2bc543393f46c286eb
Axel <axel.buee@epita.fr>
2021-01-25 11:38:19 +0100

    Started Exercice 2.2.3

---
+++ Source/Piece.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Source
+{
+    public class Piece
+    {
+        string[][] Tablero;
+        public Piece(string piece)
+        {
+            //TODO
+            /*
+            string[][] Tablero = new string[3][];
+            for (int i = 0; i < Tablero.GetLength(0); i++)
+            {
+                Tablero[i] = new string[3];
+            }
+
+
+            var rows = piece.Split('\n');*/
+        }
+
+        public interface Grid
+        {
+            int Rows();
+            int Columns();
+            char CellAt(int row, int col);
+        }
+    }
+}
